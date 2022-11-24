3188adeeca438096c731a46516f333d417f1abbe
Pelagein <68019687+Pelagein@users.noreply.github.com>
2022-11-24 21:46:25 +0300

    Добавьте файлы проекта.

---
+++ UnitLearn/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово в котором хотите заменить буквы:");
+            string Word = Console.ReadLine();
+            Console.WriteLine("Введите букву для замены:");
+            char OldLetter = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Введите новую букву:");
+            char NewLetter = Convert.ToChar(Console.ReadLine());
+            Replace replace = new Replace();
+            Word=replace.MethodLetterReplace(Word,OldLetter,NewLetter);
+            Console.WriteLine($"Результат замены: {Word} ");
+        }
+    }
+}
---
+++ UnitLearn/Replace.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitLearn
+{
+    public class Replace
+    {
+        public string MethodLetterReplace(string Word,char OldLetter,char NewLetter) 
+        {
+            string NewWord="";
+            foreach (char E in Word) 
+            {
+                if (E == OldLetter) { NewWord += NewLetter; }
+                else { NewWord += E; }
+            }
+            return NewWord;
+        }
+    }
+}
